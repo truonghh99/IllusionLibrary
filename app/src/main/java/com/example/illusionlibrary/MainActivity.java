5ad5fd440b6fb75abc814a86b9fc83b244c15ef1
truonghh99 <truonghh@beloit.edu>
2021-03-10 15:02:29 -0800

    Add toolbar to main activity

--- app/src/main/java/com/example/illusionlibrary/MainActivity.java
+++ app/src/main/java/com/example/illusionlibrary/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.illusionlibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
