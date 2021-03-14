4c0cc10a5e623c8a7c88c49b7f1ec71bf374d2ed
truonghh99 <truonghh@beloit.edu>
2021-03-14 00:01:22 -0800

    Fixed synchronization issue - adapter successfully renders images

--- app/src/main/java/com/example/illusionlibrary/fragments/LibraryFragment.java
+++ app/src/main/java/com/example/illusionlibrary/fragments/LibraryFragment.java
@@ -2,6 +2,7 @@ package com.example.illusionlibrary.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -15,8 +16,16 @@ import com.example.illusionlibrary.R;
 import com.example.illusionlibrary.adapters.ImageAdapter;
 import com.example.illusionlibrary.databinding.FragmentLibraryBinding;
 import com.example.illusionlibrary.models.Image;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,7 +34,8 @@ import java.util.List;
  */
 public class LibraryFragment extends Fragment {
 
-    private List<Image> images;
+    private static final String TAG = "LibraryFragment";
+    private List<Image> images = new ArrayList<>();
     private RecyclerView rvLibrary;
     private ImageAdapter adapter;
     public LibraryFragment() {
@@ -47,14 +57,34 @@ public class LibraryFragment extends Fragment {
 
         FragmentLibraryBinding fragmentLibraryBinding = FragmentLibraryBinding.inflate(getLayoutInflater());
 
-        Image.getAllImages();
-        Log.d("COUNT", String.valueOf(Image.images.size()));
+        getAllImages();
         // Set up adapter & recycler view
         rvLibrary = fragmentLibraryBinding.rvLibrary;
-        adapter = new ImageAdapter(getActivity(), Image.images);
+        adapter = new ImageAdapter(getActivity(), images);
         rvLibrary.setLayoutManager(new GridLayoutManager(getActivity(), 3));
         rvLibrary.setAdapter(adapter);
 
         return fragmentLibraryBinding.getRoot();
+    }
+
+    public void getAllImages() {
+        DatabaseReference mDatabase;
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.child("image").child("1").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public synchronized void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                }
+                else {
+                    Log.d("firebase", String.valueOf(task.getResult().getValue()));
+                    Map<String,String> td=(HashMap<String, String>)task.getResult().getValue();
+                    Image image = new Image(td.get("imageName"),td.get("imageLink"));
+                    images.add(image);
+                    adapter.notifyDataSetChanged();
+                    Log.e(TAG, String.valueOf(images.size()));
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/illusionlibrary/models/Image.java
+++ app/src/main/java/com/example/illusionlibrary/models/Image.java
@@ -4,6 +4,7 @@ import android.util.Log;
 
 import androidx.annotation.NonNull;
 
+import com.example.illusionlibrary.adapters.ImageAdapter;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
@@ -18,8 +19,6 @@ import java.util.Map;
 public class Image {
     public String imageName;
     public String imageLink;
-    public static List<Image> images = new ArrayList<>();
-
 
     public Image() {
         // Default constructor required for calls to DataSnapshot.getValue(User.class)
@@ -28,26 +27,6 @@ public class Image {
     public Image(String name, String link) {
         this.imageName = name;
         this.imageLink = link;
-    }
-
-    public static void getAllImages() {
-        DatabaseReference mDatabase;
-        mDatabase = FirebaseDatabase.getInstance().getReference();
-        mDatabase.child("image").child("1").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                if (!task.isSuccessful()) {
-                    Log.e("firebase", "Error getting data", task.getException());
-                }
-                else {
-                    Log.d("firebase", String.valueOf(task.getResult().getValue()));
-                    Map<String,String> td=(HashMap<String, String>)task.getResult().getValue();
-                    Image image = new Image(td.get("imageName"),td.get("imageLink"));
-                    images.add(image);
-                }
-            }
-        });
-        for (int i = 0; i < 100000; ++i);
     }
 
     public String getImageName() {
